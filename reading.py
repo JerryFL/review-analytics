ca0f69e63d76468fbda3295c79f912854275a4b9
JerryFL <3573764836@qq.com>
2020-01-15 17:08:20 +0800

    new

--- reading.py
+++ reading.py
@@ -12,4 +12,11 @@ sumlen = 0
 for d in data:
 	sumlen = sumlen + len(d)
 	len(d)
-    print(sunlen)
+    print(sumlen)
+print('留言的平均长度为'，sumlen/len（data)）
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有'， len(new), '笔留言长度小于100')
