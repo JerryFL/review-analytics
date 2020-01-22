e7069c1981c5e70855fbae740fa27d1d30563266
JerryFL <3573764836@qq.com>
2020-01-22 15:46:12 +0800

    wordcount

--- reading.py
+++ reading.py
@@ -6,17 +6,73 @@ with open('review.txt', 'r') as f:
         count += 1
         if count % 1000 == 0:
         	print(len(data))
-print(len(data))
 
-sumlen = 0 
-for d in data:
-	sumlen = sumlen + len(d)
-	len(d)
-    print(sumlen)
-print('留言的平均长度为'，sumlen/len（data)）
 
-new = []
+print(data[0])
+# 文字记数
+wc = {}#wordcount
 for d in data:
-	if len(d) < 100:
-		new.append(d)
-print('一共有'， len(new), '笔留言长度小于100')
+	words = d.split(' ') #split可以默认跳过多个空格
+for word in words:
+    if word in wc:
+        wc[word] += 1
+    else:
+        wc[word] = 1
+        print('这个字没有出现过') # 新增新的key进入word字典 
+print(wc[word])       
+
+for word in wc:
+    if wc[word] > 100:
+        print(word, wc[word])        
+print(len(wc))
+
+
+while True:
+	word = input('请输入您想查询出的字：')
+	if word == 'q':
+		break
+    if word in wc:
+        print(word, '这个字出现的次数为：'， wc[word])
+    else: 
+    	print('这个字没有出现过哦')
+
+    	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ #sumlen = 0 
+ #for d in data:
+	 #sumlen = sumlen + len(d)
+	 #len(d)
+     #print(sumlen)
+ #print('留言的平均长度为'，sumlen/len（data)）
+
+ #new = []
+ #for d in data:
+	 #if len(d) < 100:
+		#new.append(d)
+ #print('一共有'， len(new), '笔留言长度小于100')
+
+ #good = []
+ #for d in data:
+	 #if 'good' in d：
+	     #good.append(d)
+ #print('一共有'，len(good), '笔留言')
